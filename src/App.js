ddd37e321ebb7319a29674174ec92804490389f2
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2024-04-01 23:25:23 +0500

    Aima and Sarim added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Maryam Abdul Ghafoor Best Teacher</h1>
-        <h1>Aima Shahid Best TA</h1>
+        <div>
+        <h1>Best Professor: Dr. Maryam Abdul Ghafoor</h1>
         <img src="https://sbasse.lums.edu.pk/sites/default/files/styles/image_crop_3_2_860x573/public/2020-12/Maryam%20Abdul%20Ghafoor.jpg?h=b16328c1"/>
+        <h1>with Best TA squad starring: </h1>
+        <img src="https://i.ibb.co/6DGyk6d/Aima.png" alt="Aima" border="0"/>
+        <img src="https://i.ibb.co/SNXzP5Q/image.png" alt="Sarim" border="0"/>
+        
+        </div>
       </header>
     </div>
   );
