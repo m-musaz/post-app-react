be86a506c815a10f80137d57d92c8581e88231b1
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2024-04-01 23:28:10 +0500

    Final

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
       <header className="App-header">
         <div>
         <h1>Best Professor: Dr. Maryam Abdul Ghafoor</h1>
-        <img src="https://sbasse.lums.edu.pk/sites/default/files/styles/image_crop_3_2_860x573/public/2020-12/Maryam%20Abdul%20Ghafoor.jpg?h=b16328c1"/>
+        <img style={{width:"40px",height:"auto"}} src="https://sbasse.lums.edu.pk/sites/default/files/styles/image_crop_3_2_860x573/public/2020-12/Maryam%20Abdul%20Ghafoor.jpg?h=b16328c1"/>
         <h1>with Best TA squad starring: </h1>
         <img src="https://i.ibb.co/6DGyk6d/Aima.png" alt="Aima" border="0"/>
         <img src="https://i.ibb.co/SNXzP5Q/image.png" alt="Sarim" border="0"/>
+        <img src="https://i.ibb.co/6bHxwgp/image.png" alt="Mohtashim" border="0"/>
         
         </div>
       </header>
